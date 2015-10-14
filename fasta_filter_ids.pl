b5c3ca199b63c5d07ade5ed22db6e5c6d6d01688
mccrowjp <john.mccrow@gmail.com>
2015-10-14 14:06:24 -0700

    input multiple files

--- fasta_filter_ids.pl
+++ fasta_filter_ids.pl
@@ -1,16 +1,14 @@
 #!/usr/bin/env perl
 use strict;
 
-my $idfile = shift;
-my $fafile = shift;
+my ($idfile, @fastalist) = @ARGV;
 
 my %ids;
-my $read;
 my %foundids;
 my $totalfa = 0;
 
-unless($idfile && $fafile) {
-    die "Usage: $0 [id file] [fasta file]\noutput: fasta of only IDs in first column of id_file\n";
+unless($idfile && scalar(@fastalist) > 0) {
+    die "Usage: $0 [id file] [FASTA file(s) ...]\noutput: FASTA of only IDs in first column of id_file\n";
 }
 
 if($idfile =~ /\.gz$/) {
@@ -18,6 +16,7 @@ if($idfile =~ /\.gz$/) {
 } else {
     open(IN, $idfile) or die "Unable to open file $idfile\n";
 }
+
 while(<IN>) {
     chomp;
     my ($id) = split(/[\t\s]/);
@@ -27,25 +26,30 @@ close(IN);
 
 print STDERR "ID file: IDs = ".scalar(keys %ids)."\n";
 
-if($fafile =~ /\.gz$/) {
-    open(IN, "gunzip -c $fafile 2>/dev/null |");
-} else {
-    open(IN, $fafile) or die "Unable to open file $fafile\n";
-}
-while(<IN>) {
-    if(/^>/) {
-        $totalfa++;
-        $read = 0;
-        my ($id) = split(/[\t\s]/, $');  #');
-        if($ids{$id}) {
-            $foundids{$id} = 1;
-            $read = 1;
+foreach my $fafile (@fastalist) {
+    if($fafile =~ /\.gz$/) {
+        open(IN, "gunzip -c $fafile 2>/dev/null |");
+    } else {
+        open(IN, $fafile) or die "Unable to open file $fafile\n";
+    }
+    
+    my $read = 0;
+    while(<IN>) {
+        if(/^>/) {
+            $totalfa++;
+            $read = 0;
+            my ($id) = split(/[\t\s]/, $');  #');
+            if($ids{$id}) {
+                $foundids{$id} = 1;
+                $read = 1;
+            }
+        }
+        if($read) {
+            print $_;
         }
     }
-    if($read) {
-        print $_;
-    }
+    close(IN);
 }
-close(IN);
 
-print STDERR "Fasta file: total = ".$totalfa.", IDs found = ".scalar(keys %foundids)."\n";
+print STDERR "Fasta file(s): IDs = $totalfa\n";
+print STDERR "Matching IDs found = ".scalar(keys %foundids)."\n";
