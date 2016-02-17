50c8414bbcad7d76e5c8f5726741663b36fec939
mccrowjp <john.mccrow@gmail.com>
2016-02-17 15:44:37 -0800

    update

--- bam_pid_summary.pl
+++ bam_pid_summary.pl
@@ -2,16 +2,33 @@
 use strict;
 
 my $bamfile = shift;
+my $reffile = shift;
+
+my $help = <<HELP;
+Usage: $0 [BAM file] ([ref FASTA file])
+
+  Input:
+    If BAM file has '=' in place of identical bases (from samtools calmd), then only BAM is needed.
+    Otherwise, optional ref FASTA file parameter is needed for the conversion.
+
+  Output:
+    Tab delimited table of read counts, and average percent identity for each ID
+
+HELP
 
 unless($bamfile) {
-    die "Usage: $0 [BAM file]\n";
+    die $help;
 }
 
 my %readcount;
 my %totlen;
 my %identlen;
 
-open(IN, "samtools view -F 4 $bamfile 2>/dev/null |");
+if(length($reffile) > 0) {
+    open(IN, "samtools calmd -eu $bamfile $reffile | samtools view -F 4 - 2>/dev/null |");
+} else {
+    open(IN, "samtools view -F 4 $bamfile 2>/dev/null |");
+}
 while(<IN>) {
     chomp;
     my ($qname, $flag, $rname, $pos, $mapq, $cigar, $rnext, $pnext, $tlen, $seq, $qual) = split(/\t/);
