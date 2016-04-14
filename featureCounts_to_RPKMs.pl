cd071de09446b5391cec76df1d95b96d4f3e2f47
mccrowjp <john.mccrow@gmail.com>
2016-04-14 11:16:48 -0700

    Initial commit

---
+++ featureCounts_to_RPKMs.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use strict;
+
+my $incounts = shift;
+
+unless($incounts) {
+    die "Usage: $0 [FeatureCounts output table]\n";
+}
+
+my %idlen;
+my %idhead;
+my %idlibcount;
+my %libtotal;
+my @headnames;
+my @libnames;
+	
+open(IN, $incounts) or die "Unable to open file $incounts\n";
+my $fl = 1;
+while(<IN>) {
+    chomp;
+    unless(/^\#/) {
+        my ($id, $chr, $s, $e, $strand, $len, @counts) = split(/\t/);
+        if($fl) {
+            unless($id eq 'Geneid') {
+                die "Unrecognized featureCounts table format\n";
+            }
+            @headnames = ($id, $chr, $s, $e, $strand, $len);
+            @libnames = @counts;
+            
+        } else {
+            $idhead{$id} = join("\t", ($id, $chr, $s, $e, $strand, $len));
+            $idlen{$id} = $len;
+            for(my $i=0; $i<scalar(@counts); $i++) {
+                $idlibcount{$id}{$i} += $counts[$i];
+                $libtotal{$i} += $counts[$i];
+            }
+        }
+        $fl = 0;
+    }
+}
+close(IN);
+
+print join("\t", (@headnames, @libnames))."\n";
+
+foreach my $id (sort keys %idlen) {
+    print $idhead{$id};
+    for(my $i=0; $i<scalar(@libnames); $i++) {
+        if($libtotal{$i} * $idlen{$id} > 0) {
+            printf "\t%.2f", (10**9 * $idlibcount{$id}{$i}) / ($libtotal{$i} * $idlen{$id});
+        } else {
+            print "\t0";
+        }
+    }
+    print "\n";
+}
