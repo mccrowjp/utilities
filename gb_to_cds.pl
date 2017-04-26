3af25501aeb5d75bfa149aa5aaf61668db6c0aac
mccrowjp <john.mccrow@gmail.com>
2017-04-26 16:35:33 -0700

    ignore pseudo CDS

--- gb_to_cds.pl
+++ gb_to_cds.pl
@@ -16,27 +16,29 @@ while(my $objseq = $objseqio->next_seq) {
     my @cds_features = grep { $_->primary_tag eq 'CDS' } $objseq->get_SeqFeatures;
     
     foreach my $objfeat (@cds_features) {
-        my ($protid, @others) = $objfeat->get_tag_values('protein_id');
-        my ($prod_ann) = $objfeat->get_tag_values('product');
-        my ($locus, @others);
+        unless($objfeat->has_tag('pseudo')) {
+            my ($protid, @others) = $objfeat->get_tag_values('protein_id');
+            my ($prod_ann) = $objfeat->get_tag_values('product');
+            my ($locus, @others);
         
-        if($objfeat->has_tag('gene')) {
-            ($locus, @others) = $objfeat->get_tag_values('gene');
-            if(scalar(@others) > 0) {
-                print STDERR "Multiple gene values for CDS on $acc\n";
+            if($objfeat->has_tag('gene')) {
+                ($locus, @others) = $objfeat->get_tag_values('gene');
+                if(scalar(@others) > 0) {
+                    print STDERR "Multiple gene values for CDS on $acc\n";
+                }
+            } elsif($objfeat->has_tag('locus_tag')) {
+                ($locus, @others) = $objfeat->get_tag_values('locus_tag');
+                if(scalar(@others) > 0) {
+                    print STDERR "Multiple locus_tag values for CDS on $acc\n";
+                }
             }
-        } elsif($objfeat->has_tag('locus_tag')) {
-            ($locus, @others) = $objfeat->get_tag_values('locus_tag');
-            if(scalar(@others) > 0) {
-                print STDERR "Multiple locus_tag values for CDS on $acc\n";
-            }
-        }
         
-        if(length($locus) > 0) {
-            printf ">%s %s %s\n", $locus, $protid, $prod_ann;
-            print $objfeat->spliced_seq->seq."\n";
-        } else {
-            print STDERR "No gene/locus_tag found for CDS on $acc\n";
+            if(length($locus) > 0) {
+                printf ">%s %s %s\n", $locus, $protid, $prod_ann;
+                print $objfeat->spliced_seq->seq."\n";
+            } else {
+                print STDERR "No gene/locus_tag found for CDS on $acc\n";
+            }
         }
     }
 }
