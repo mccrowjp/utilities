5dfeaf9f8916c8345e5cc506682448840a53d902
mccrowjp <john.mccrow@gmail.com>
2017-12-12 12:21:09 -0800

    report total, truncated, removed

--- fasta_truncate_peptide_to_first_M.pl
+++ fasta_truncate_peptide_to_first_M.pl
@@ -5,16 +5,26 @@ my $head;
 my $lasthead;
 my $str;
 
+my $count_total = 0;
+my $count_trunc = 0;
+my $count_remove = 0;
+
 sub eachseq {
-    unless($str =~ /^[Mm]/) {
-        if($str =~ /^[^Mm]+([Mm].+)$/) {
-            $str = $1;
-        } else {
-            $str = "";
-        }
-    }
     if(length($str) > 0) {
-        print ">".$lasthead."\n".$str."\n";
+        $count_total++;
+        
+        unless($str =~ /^[Mm]/) {
+            if($str =~ /^[^Mm]+([Mm].+)$/) {
+                $str = $1;
+                $count_trunc++;
+            } else {
+                $str = "";
+                $count_remove++;
+            }
+        }
+        if(length($str) > 0) {
+            print ">".$lasthead."\n".$str."\n";
+        }
     }
 }
 
@@ -45,5 +55,8 @@ while(<IN>) {
     }
 }
 eachseq();
-
 close(IN);
+
+printf STDERR "Total    : %d\n", $count_total;
+printf STDERR "Truncated: %d (%.1f \%)\n", $count_trunc, 100.0*$count_trunc/$count_total;
+printf STDERR "Removed  : %d (%.1f \%)\n", $count_remove, 100.0*$count_remove/$count_total;
