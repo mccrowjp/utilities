ebc63d68c1b7d819a89d59122c18e9419fa313fb
mccrowjp <john.mccrow@gmail.com>
2018-01-09 10:42:59 -0800

    m8_besthit gzip

--- m8_besthit.pl
+++ m8_besthit.pl
@@ -9,16 +9,21 @@ unless($infile) {
     die "Usage: $0 [Blast m8 file]\n  output: best single hit to each query sequence\n";
 }
 
-open(IN, $infile) or die "Unable to open file $infile\n";
+if($infile =~ /\.gz$/) {
+    open(IN, "gunzip -c $infile 2>/dev/null |");
+} else {
+    open(IN, $infile) or die "Unable to open file $infile\n";
+}
 
 my $lastid;
 while(<IN>) {
     chomp;
     unless(/^\#/) {
-	my ($qid, $sid, $pid, $len, $mm, $go, $qs, $qe, $ss, $se, $e, $bs) = split(/\t/);
-	if($bs > $bestbs{$qid}) {
-	    $bestbs{$qid} = $bs;
-	    $bestline{$qid} = $_;
+        my ($qid, $sid, $pid, $len, $mm, $go, $qs, $qe, $ss, $se, $e, $bs) = split(/\t/);
+        if($bs > $bestbs{$qid}) {
+            $bestbs{$qid} = $bs;
+            $bestline{$qid} = $_;
+        }
     }
 }
 close(IN);
